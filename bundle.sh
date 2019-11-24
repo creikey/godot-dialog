faff76dab119c287d1ae1d2fecb49c61efa28b59
Cameron Reikes <cameronreikes@gmail.com>
2019-11-24 02:43:02 -0800

    Remove web export

--- bundle.sh
+++ bundle.sh
@@ -7,16 +7,13 @@ fi
 fi
 
 rm -r exports
-mkdir -p exports/web
 mkdir -p exports/linux
 mkdir -p exports/windows
 mkdir -p exports/mac
-godot-headless --path editor --export web "$(readlink -f ./exports/web/index.html)"
 godot-headless --path editor --export windows "$(readlink -f ./exports/windows/godot-dialog.exe)"
 godot-headless --path editor --export linux "$(readlink -f ./exports/linux/godot-dialog.x86_64)"
 godot-headless --path editor --export mac "$(readlink -f ./exports/mac/Godot\ Dialog)"
 if [ "$?" == "0" ]; then
-	butler -i ~/.config/itch/creikey push exports/web creikey/godot-dialog:web 
 	butler -i ~/.config/itch/creikey push exports/windows creikey/godot-dialog:windows
 	butler -i ~/.config/itch/creikey push exports/linux creikey/godot-dialog:linux
 
